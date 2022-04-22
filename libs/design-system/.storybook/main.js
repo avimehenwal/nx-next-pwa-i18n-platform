6e5ee3e68b310013eca941d833a00456d09136b4
avimehenwal <avi.mehanwal@gmail.com>
2022-04-22 20:30:02 +0200

    feat(ds): storybook to design-system live previews
    Cannot find module '@storybook/angular/package.json'
    use react-storybook and NOT angular-storybook

--- libs/design-system/.storybook/main.js
+++ libs/design-system/.storybook/main.js
@@ -3,12 +3,14 @@ const rootMain = require('../../../.storybook/main');
 module.exports = {
   ...rootMain,
 
+  core: { ...rootMain.core, builder: 'webpack5' },
+
   stories: [
     ...rootMain.stories,
     '../src/lib/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: [...rootMain.addons],
+  addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
   webpackFinal: async (config, { configType }) => {
     // apply any global webpack configs that might have been specified in .storybook/main.js
     if (rootMain.webpackFinal) {

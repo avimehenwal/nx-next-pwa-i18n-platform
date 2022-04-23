c73c4e0a8277c59a9f774f6a941b6507b346e337
avimehenwal <avi.mehanwal@gmail.com>
2022-04-24 01:08:43 +0200

    feat(nx): generate files from nx
    - rename and variabalize file names
    - pass context variable values to ESJ templates
    - can have multiple files inside fileTree
    
    npx nx workspace-generator my-generator from-CLI --dry-run

--- tools/generators/my-generator/index.ts
+++ tools/generators/my-generator/index.ts
@@ -1,4 +1,11 @@
-import { Tree, formatFiles, installPackagesTask, logger } from '@nrwl/devkit';
+import {
+  Tree,
+  formatFiles,
+  installPackagesTask,
+  logger,
+  generateFiles,
+  joinPathFragments,
+} from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
 interface IGeneratorOptions {
@@ -9,11 +16,32 @@ export default async function (tree: Tree, schema: IGeneratorOptions) {
   logger.info(`${tree.root}`);
   logger.info(`Hello ${schema.name} attendes!!`);
 
-  // scaffolds new library with 11 NEW files and 2 Updated filesa
-  await libraryGenerator(tree, { name: schema.name });
+  generateFiles(
+    // virtual file system
+    tree,
+
+    // the location where the template files are
+    joinPathFragments(__dirname, './files'),
+
+    // where the files should be generated
+    './articles',
+
+    // the variables to be substituted in the template
+    {
+      name: schema.name,
+      normalizedName: schema.name,
+      // excerpt: schema.excerpt || '',
+      // creationDate: new Date().toISOString(),
+    }
+  );
+
   await formatFiles(tree);
-  return () => {
-    // install if any new dependency is added to package.json
-    installPackagesTask(tree);
-  };
+
+  // scaffolds new library with 11 NEW files and 2 Updated filesa
+  // await libraryGenerator(tree, { name: schema.name });
+  // await formatFiles(tree);
+  // return () => {
+  // install if any new dependency is added to package.json
+  // installPackagesTask(tree);
+  // };
 }

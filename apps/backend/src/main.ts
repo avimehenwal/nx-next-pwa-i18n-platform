cecd2852fc2740e4a515d38cc67ce8d02a6b8473
avimehenwal <avi.mehanwal@gmail.com>
2022-04-26 21:39:49 +0200

    fix(nest): make api basePrefix work with swagger docs
    WARN [DocumentBuilder] The "setBasePath" method has been deprecated.
    Now, a global prefix is populated automatically.
    If you want to ignore it, take a look here: https://docs.nestjs.com/recipes/swagger#global-prefix.
    Alternatively, you can use "addServer" method to set up multiple different paths

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,19 +10,19 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3333;
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
+    // .setBasePath(globalPrefix)
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('openapi', app, document);
-
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
 
   await app.listen(port);
   Logger.log(

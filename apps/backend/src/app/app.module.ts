13aada7f1204f01f16cfa90d067611fca88adc14
avimehenwal <avi.mehanwal@gmail.com>
2022-04-27 13:29:04 +0200

    feat(nest): scaffold new user service

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatApiModule } from './cat-api/cat-api.module';
 import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [CatApiModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, UserService],
 })
 export class AppModule {}

84c3736d83015baec461a925afd81c7048d56fc6
avimehenwal <avi.mehanwal@gmail.com>
2022-04-27 13:24:13 +0200

    feat(orm): scaffold new prisma service inside app
    PrismaService that takes care of instantiating PrismaClient and connecting to your database.

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatApiModule } from './cat-api/cat-api.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [CatApiModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}

d3a31c5bb6b2f374f9c111a61faca5dcd5c62f65
avimehenwal <avi.mehanwal@gmail.com>
2022-04-27 13:32:05 +0200

    feat(nest): scaffold post DB service

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppService } from './app.service';
 import { CatApiModule } from './cat-api/cat-api.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
 
 @Module({
   imports: [CatApiModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService, UserService],
+  providers: [AppService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
